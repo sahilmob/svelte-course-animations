0ced9bac08f34738560b68dda0ff93f61cc66985
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-22 21:45:47 +0300

    use transitions

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { tweened } from "svelte/motion";
   import { cubicIn } from "svelte/easing";
   import Spring from "./Spring.svelte";
+  import { fade, fly, slide, scale } from "svelte/transition";
 
   const progress = tweened(0, {
     delay: 0,
@@ -11,8 +12,39 @@
   setTimeout(() => {
     progress.set(0.5);
   }, 1500);
+
+  let boxes = [];
+
+  function addBox() {
+    boxes = [...boxes, Math.random()];
+  }
+
+  function discard(value) {
+    boxes = boxes.filter(el => el !== value);
+  }
 </script>
+
+<style>
+  div {
+    width: 10rem;
+    height: 10rem;
+    background: #ccc;
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    padding: 1rem;
+  }
+</style>
 
 <!-- <progress value={$progress} /> -->
 
-<Spring />
+<!-- <Spring /> -->
+
+<button on:click={addBox}>Add box</button>
+{#each boxes as box (box)}
+  <div
+    transition:fly={{ easing: cubicIn, x: -300 }}
+    on:click={discard.bind(this, box)}>
+     {box}
+  </div>
+{/each}
