178d6a4ec7e6080b0abdbda541562d63023d03bd
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-22 21:51:08 +0300

    more transitions

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import Spring from "./Spring.svelte";
   import { fade, fly, slide, scale } from "svelte/transition";
 
+  let showParagraph = false;
   const progress = tweened(0, {
     delay: 0,
     duration: 700,
@@ -40,11 +41,18 @@
 
 <!-- <Spring /> -->
 
+<button on:click={() => (showParagraph = !showParagraph)}>Toggle</button>
 <button on:click={addBox}>Add box</button>
-{#each boxes as box (box)}
-  <div
-    transition:fly={{ easing: cubicIn, x: -300 }}
-    on:click={discard.bind(this, box)}>
-     {box}
-  </div>
-{/each}
+{#if showParagraph}
+  <p transition:fly={{ x: 300 }}>Can you see me?</p>
+{/if}
+
+{#if showParagraph}
+  {#each boxes as box (box)}
+    <div
+      transition:fly={{ easing: cubicIn, x: -300 }}
+      on:click={discard.bind(this, box)}>
+       {box}
+    </div>
+  {/each}
+{/if}
