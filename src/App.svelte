82588ce64681c994a2da320cbac7927e4cced1cd
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-22 21:52:18 +0300

    use in and out

--- src/App.svelte
+++ src/App.svelte
@@ -44,7 +44,7 @@
 <button on:click={() => (showParagraph = !showParagraph)}>Toggle</button>
 <button on:click={addBox}>Add box</button>
 {#if showParagraph}
-  <p transition:fly={{ x: 300 }}>Can you see me?</p>
+  <p in:fade out:fly={{ x: 300 }}>Can you see me?</p>
 {/if}
 
 {#if showParagraph}
